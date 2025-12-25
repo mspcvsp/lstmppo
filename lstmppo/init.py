7a4ad5427f9b6bae1f5fa6c26a643e485ef199d1
Sean McCarty <mspcvsp@gmail.com>
2025-12-25 13:27:40 -0500

    Updated

--- lstmppo/init.py
+++ lstmppo/init.py
@@ -10,51 +10,31 @@ import numpy as np
 
 
 @dataclass
-class Args:
-    exp_name: str = "RLTestDrive"
-    """the name of this experiment"""
-    seed: int = 351530767
-    """seed of the experiment"""
-    torch_deterministic: bool = True
-    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+class PPOConfig:
     cuda: bool = True
-    """if toggled, cuda will be enabled by default"""
-    track: bool = False
-    """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
-    """the wandb's project name"""
-    wandb_entity: Optional[str] = None
-    """the entity (team) of wandb's project"""
-    capture_video: bool = False
-    """whether to capture videos of the agent performances (check out `videos` folder)"""
-
-    # Algorithm specific arguments
-    env_id: str = "CartPole-v1"
-    """the id of the environment"""
-    total_timesteps: int = 100000
+    """GPU/CPU toggle"""
+    env_id: str = "PositionOnlyCartPoleEasy"
+    """Environment identifier"""
+    exp_name: str = "RLWarmup"
+    """Experiment name"""
+    total_steps: int = 200_000
     """total timesteps of the experiments"""
-    learning_rate: float = 2.5e-4
-    """the learning rate of the optimizer"""
-    num_envs: int = 4
-    """the number of parallel game environments"""
-    horizon: int = 128
-    """the number of steps to run in each environment per policy rollout"""
-    anneal_lr: bool = True
-    """Toggle learning rate annealing for policy and value networks"""
+    num_envs: int = 16
+    """ Number of environments """
+    rollout_steps: int = 128
+    """ Horizon"""
     gamma: float = 0.99
-    """the discount factor gamma"""
+    """ Discount factor"""
     gae_lambda: float = 0.95
-    """the lambda for the general advantage estimation"""
-    num_minibatches: int = 4
-    """the number of mini-batches"""
-    update_epochs: int = 16
-    """the K epochs to update the policy"""
-    norm_adv: bool = True
-    """Toggles advantages normalization"""
+    """ Generalized Advantage Estimate (GAE) lambda"""
+    learning_rate: float = 3e-4
+    """Learning rate"""
     clip_coef: float = 0.2
-    """the surrogate clipping coefficient"""
-    clip_vloss: bool = True
-    """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
+    """ Clip coefficient"""
+    update_epochs: int = 4
+    """ Number of update eophics"""
+    batch_envs: int = 4 
+    """number of envs per recurrent minibatch"""
     ent_coef: float = 0.01
     """Fixed coefficient of the entropy if annealing is disabled"""
     anneal_entropy_flag: bool = True
@@ -67,24 +47,18 @@ class Args:
     """coefficient of the value function"""
     max_grad_norm: float = 0.5
     """the maximum norm for the gradient clipping"""
-    target_kl: Optional[float] = None
-    """the target KL divergence threshold"""
-
-    # to be filled in runtime
-    batch_size: int = 0
-    """the batch size (computed in runtime)"""
-    minibatch_size: int = 0
-    """the mini-batch size (computed in runtime)"""
-    num_iterations: int = 0
-    """the number of iterations (computed in runtime)"""
-
+    hidden_size: int = 64
+    """LSTM hidden size"""
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    """the name of this experiment"""
+    seed: int = 351530767
+    """seed of the experiment"""
+    torch_deterministic: bool = True
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
 
 def initialize(seconds_since_epoch=None):
     
-    params = tyro.cli(Args)
-    params.batch_size = int(params.num_envs * params.horizon)
-    params.minibatch_size = int(params.batch_size // params.num_minibatches)
-    params.num_iterations = params.total_timesteps // params.batch_size
+    params = tyro.cli(PPOConfig)
 
     if seconds_since_epoch is None:
         seconds_since_epoch = int(time.time())
@@ -93,19 +67,6 @@ def initialize(seconds_since_epoch=None):
         f"{params.env_id}__{params.exp_name}__{params.seed}__" +\
         f"{seconds_since_epoch}"
 
-    if params.track:
-
-        wandb.init(
-            project=params.wandb_project_name,
-            entity=params.wandb_entity,
-            sync_tensorboard=True,
-            config=vars(params),
-            name=params.run_name,
-            monitor_gym=True,
-            save_code=True,
-        )
-
-    # TRY NOT TO MODIFY: seeding
     random.seed(params.seed)
     np.random.seed(params.seed)
     torch.manual_seed(params.seed)
